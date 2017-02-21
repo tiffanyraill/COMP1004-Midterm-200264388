f8f6b96c4b3332ebfee2e943728cfc0791326a94
Tiffany Raill <tiffany.raill@mygeorgian.ca>
2017-02-20 20:12:17 -0500

    Add project files.

---
+++ COMP1004-MidTerm-200264388/RaceForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP1004_MidTerm_200264388
+{
+    public partial class RaceForm : Form
+    {
+        public RaceForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
